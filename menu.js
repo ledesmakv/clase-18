d2a6cecbea193310a67b1935fe2ba06082cc5c38
Karina <ledesmakv@gmail.com>
2019-05-16 21:20:41 -0300

    breve explicación de la función aplicada en html

--- menu.js
+++ menu.js
@@ -1,4 +1,7 @@
-var toggleMenu = function () {
+// defino la función toggleMenu (a - onclick)
+function toggleMenu () {
+    // hacemos referencia a nuestro ul class 'menu' (guardamos el ID en una variable)
     var item = document.getElementById('menu');
+    // con esta linea cambiamos a la clase .open que tenemos en el CSS para aplicar transforma: translate a nuestro menú
     item.classList.toggle('open');
 }
